991d91eb2c50192a0ea19c3d2e4c4906b6b46895
Éverton M. Vieira <everton.muvi@gmail.com>
2021-12-09 16:09:50 -0300

    prototype development

--- src/main.rs
+++ src/main.rs
@@ -12,15 +12,17 @@ mod utils;
 
 fn main() {
 	let args = clip::parse();
-	let body: String;
-	if args.is_present("body") {
-		body = String::from(args.value_of("body").expect("Problem on read body argument."));
+	let body = if args.is_present("body") {
+		String::from(
+			args.value_of("body")
+				.expect("Problem on read body argument."),
+		)
 	} else {
-		let body_err_msg = "You let me as an errant soul. You must give me a body, as an argument -b / --body or environment variable PACKALL_BODY";
+		let body_err_msg = "You let me as an errant soul. You must give me a body, as an argument -b / --body or with the environment variable PACKALL_BODY";
 		let body_env = std::env::var_os("PACKALL_BODY").expect(body_err_msg);
 		let body_env = body_env.to_str().expect(body_err_msg);
-		body = format!("{}", body_env);
-	}
+		format!("{}", body_env)
+	};
 	let speed_str = args.value_of("speed").expect("You must pass a speed.");
 	let speed = speed_str.parse().expect("You must pass a valid speed.");
 	let clean = args.is_present("clean");
