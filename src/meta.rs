a69c02302552addc3436867b08d2ebaba4639894
Éverton M. Vieira <emuvi@goutlook.com.br>
2022-05-13 18:44:32 -0300

    adds times metadata of files from the OS

--- src/meta.rs
+++ src/meta.rs
@@ -1,3 +1,6 @@
+use chrono::offset::Utc;
+use chrono::DateTime;
+
 use std::path::PathBuf;
 
 pub fn add_all_meta_data(of_file: &PathBuf, destiny_dir: &PathBuf) {
@@ -8,6 +11,23 @@ pub fn add_all_meta_data(of_file: &PathBuf, destiny_dir: &PathBuf) {
   }
   if let Some(parent) = of_file.parent() {
     add_file_tree_on(&destiny_dir, &format!("{}", parent.display()));
+  }
+  if let Ok(meta) = of_file.metadata() {
+    if let Ok(created) = meta.created() {
+      let created: DateTime<Utc> = created.into();
+      let created = created.format("%Y-%m-%dT%H:%M:%S").to_string();
+      add_file_created_on(&destiny_dir, &created);
+    }
+    if let Ok(modified) = meta.modified() {
+      let modified: DateTime<Utc> = modified.into();
+      let modified = modified.format("%Y-%m-%dT%H:%M:%S").to_string();
+      add_file_modified_on(&destiny_dir, &modified);
+    }
+    if let Ok(accessed) = meta.accessed() {
+      let accessed: DateTime<Utc> = accessed.into();
+      let accessed = accessed.format("%Y-%m-%dT%H:%M:%S").to_string();
+      add_file_accessed_on(&destiny_dir, &accessed);
+    }
   }
   if let Some(old_main_meta) = find_main_meta(of_file) {
     import_main_meta_on(&destiny_dir, &old_main_meta);
@@ -20,6 +40,18 @@ pub fn add_file_name_on(destiny_dir: &PathBuf, file_name: &str) {
 
 pub fn add_file_tree_on(destiny_dir: &PathBuf, file_tree: &str) {
   add_main_meta_on(destiny_dir, "file_tree", file_tree);
+}
+
+pub fn add_file_created_on(destiny_dir: &PathBuf, file_tree: &str) {
+  add_main_meta_on(destiny_dir, "file_created", file_tree);
+}
+
+pub fn add_file_modified_on(destiny_dir: &PathBuf, file_tree: &str) {
+  add_main_meta_on(destiny_dir, "file_modified", file_tree);
+}
+
+pub fn add_file_accessed_on(destiny_dir: &PathBuf, file_tree: &str) {
+  add_main_meta_on(destiny_dir, "file_accessed", file_tree);
 }
 
 pub fn import_main_meta_on(destiny_dir: &PathBuf, from_main_meta_file: &PathBuf) {
